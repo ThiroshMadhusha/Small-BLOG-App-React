5e22fc7617aab44802f5ea4999aa965e0d41a303
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-07-14 18:19:42 +0530

    Create User Model class

---
+++ backend/models/User.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+});
+
+export default mongoose.model("User", UserSchema);
+// export all data for the mongoDB
+// In here we add "User" -->> In mongoDB Display it in "users"
