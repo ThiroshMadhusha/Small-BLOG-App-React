ae9f02c2c70b94aeb3623a6f4198f8d62775e981
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-07-14 18:19:25 +0530

    complete user router paths

---
+++ backend/routes/user-routes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { getAllUser, signin, signup } from "../controllers/user-controller";
+const router = express.Router();
+
+// Create All User Routes
+router.get("/", getAllUser);
+router.post("/signup", signup);
+router.post("/signin", signin);
+
+export default router;
