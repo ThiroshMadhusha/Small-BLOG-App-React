70c2975bd910cb1f746fcc7113a44406e5277943
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-07-14 18:18:28 +0530

    create backend api

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,24 @@
+// Import Express JS
 import express from "express";
-
 const app = express();
 
-app.use("/api", (req, res, next) => {
-  res.send("Hello World");
-});
+// Send Request For Database Using JSON Server(Like Bodyparser)
+app.use(express.json())
 
-app.listen(5000);
+// Import Routers
+import router from "./routes/user-routes";
+app.use("/api/user", router);
+
+// import mongoDB database
+import mongoose from "mongoose";
+mongoose
+  .connect(
+    "mongodb+srv://admin:3OTKzhbCjdMI3I0q@cluster0.bwnn9cb.mongodb.net/Blog?retryWrites=true&w=majority"
+  )
+  .then(() => app.listen(5000))
+  .then(() =>
+    console.log("Connect To Database and Listerning to Localhost 5000")
+  )
+  .catch((err) => console.log(err));
+
+// Data base Password -->>  3OTKzhbCjdMI3I0q
