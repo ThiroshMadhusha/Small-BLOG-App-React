75f78005dfe8245268c33821505f1048ff8517d9
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-07-16 14:32:33 +0530

    install material ui and redux toolkit

--- frontend/components/AddBlog.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AddBlog = () => {
-  return (
-    <div>AddBlog</div>
-  )
-}
-
-export default AddBlog
--- frontend/components/UserBlog.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const UserBlog = () => {
-  return (
-    <div>UserBlog</div>
-  )
-}
-
-export default UserBlog
