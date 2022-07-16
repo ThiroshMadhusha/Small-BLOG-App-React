276bf05cfd244ffd0d1cedb2372b35a648a553c8
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-07-16 15:16:35 +0530

    Add router dom and redux into index file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,9 +3,20 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store";
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+// Browser Route Coming From React Router Dom
+// Provider Coming from React-Redux
