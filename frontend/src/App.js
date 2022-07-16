d92637e44ddf69d1572b2853cbe9d568172b9177
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-07-16 15:15:09 +0530

    add auth path  and components paths into app js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,33 @@
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Auth from "./components/Auth";
+import Blogs from "./components/Blogs";
+import BlogDetails from "./components/BlogDetails";
+import UserBlogs from "./components/UserBlogs";
+import AddBlogs from "./components/AddBlogs";
+import { useSelector } from "react-redux";
 
 function App() {
+  const isLoggedIn = useSelector((state) => state.isLoggedIn);
+  console.log(isLoggedIn);
+
   return (
-    <div className="App">
-      App Hello
-    </div>
+    <React.Fragment>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/blogs/add" element={<AddBlogs />} />
+          <Route path="/myBlogs" element={<UserBlogs />} />
+          <Route path="/myBlogs/:id" element={<BlogDetails />} />
+        </Routes>
+      </main>
+    </React.Fragment>
   );
 }
 
