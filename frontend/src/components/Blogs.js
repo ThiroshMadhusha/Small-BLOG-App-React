966dcff9edf6eeeb75a4effcd466eb3f219160ea
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-07-18 23:15:48 +0530

    Create Main Blogs page

--- frontend/src/components/Blogs.js
+++ frontend/src/components/Blogs.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Blog from "./Blog";
 
 const Blogs = () => {
-  return (
-    <div>Blogs</div>
-  )
-}
+  const [blogs, setBlogs] = useState();
+  const sendRequest = async () => {
+    const res = await axios
+      .get("http://localhost:5000/api/blog")
+      .catch((error) => console.log(error));
 
-export default Blogs
+    const data = await res.data;
+    return data;
+  };
+
+  useEffect(() => {
+    sendRequest().then((data) => setBlogs(data.blogs));
+  }, []);
+  console.log(blogs);
+
+  return (
+    <div>
+      {blogs &&
+        blogs.map((blog, index) => (
+          <Blog
+            title={blog.title}
+            description={blog.description}
+            imageURL={blog.imageURL}
+            userName={blog.user.name}
+          />
+        ))}
+    </div>
+  );
+};
+
+export default Blogs;
