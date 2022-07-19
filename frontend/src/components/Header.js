015bdf6d0609a2d8f41f1ca73ccf7909e00bc81d
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-07-19 22:49:47 +0530

    Complete Blog  Navigation Bar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,81 +1,97 @@
 import React, { useState } from "react";
 import {
   AppBar,
+  Typography,
+  Toolbar,
   Box,
   Button,
-  Tab,
   Tabs,
-  Toolbar,
-  Typography,
+  Tab,
 } from "@mui/material";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { authAction } from "../store";
-
+import { useStyles } from "./utils";
 const Header = () => {
+  const classes = useStyles();
+  const dispath = useDispatch();
   const isLoggedIn = useSelector((state) => state.isLoggedIn);
 
-  const [value, setvalue] = useState();
-
-  // For Logout Buttonn Using useDispatch()
-  const dispatch = useDispatch();
+  const [value, setValue] = useState();
   return (
     <AppBar
       position="sticky"
       sx={{
         background:
-          "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%);",
+          "linear-gradient(90deg, rgba(58,75,180,1) 2%, rgba(116,49,110,1) 36%, rgba(2,0,161,1) 73%, rgba(69,92,252,1) 100%)",
       }}
     >
       <Toolbar>
-        <Typography variant="h4">Thirosh BLOG</Typography>
-
+        <Typography className={classes.font} variant="h4">
+          BlogsApp
+        </Typography>
         {isLoggedIn && (
-          <Box display="flex" marginLeft={"auto"} marginRight={"auto"}>
+          <Box display="flex" marginLeft={"auto"} marginRight="auto">
             <Tabs
               textColor="inherit"
               value={value}
-              onChange={(e, val) => setvalue(val)}
+              onChange={(e, val) => setValue(val)}
             >
-              <Tab LinkComponent={Link} to="/blogs" label="All Blogs" />
-              <Tab LinkComponent={Link} to="/myBlogs" label="My Blogs" />
+              <Tab
+                className={classes.font}
+                LinkComponent={Link}
+                to="/blogs"
+                label="All Blogs"
+              />
+              <Tab
+                className={classes.font}
+                LinkComponent={Link}
+                to="/myBlogs"
+                label="My Blogs"
+              />
+              <Tab
+                className={classes.font}
+                LinkComponent={Link}
+                to="/blogs/add"
+                label="Add Blog"
+              />
             </Tabs>
           </Box>
         )}
-
         <Box display="flex" marginLeft="auto">
           {!isLoggedIn && (
             <>
+              {" "}
               <Button
+                LinkComponent={Link}
+                to="/auth"
                 variant="contained"
                 sx={{ margin: 1, borderRadius: 10 }}
                 color="warning"
-                LinkComponent={Link}
-                to="/auth"
               >
-                Sign In
+                Login
               </Button>
               <Button
+                LinkComponent={Link}
+                to="/auth"
                 variant="contained"
                 sx={{ margin: 1, borderRadius: 10 }}
                 color="warning"
-                LinkComponent={Link}
-                to="/auth"
               >
-                Sign Up
-              </Button>{" "}
+                Signup
+              </Button>
             </>
           )}
           {isLoggedIn && (
             <Button
+              onClick={() => dispath(authAction.logout())}
+              LinkComponent={Link}
+              to="/auth"
               variant="contained"
               sx={{ margin: 1, borderRadius: 10 }}
               color="warning"
-              LinkComponent={Link}
-              to="/auth"
-              onClick={() => dispatch(authAction.logout())}
             >
-              LogOut
+              Logout
             </Button>
           )}
         </Box>
